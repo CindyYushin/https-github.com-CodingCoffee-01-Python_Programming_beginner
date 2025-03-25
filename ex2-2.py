16967ce63d17b8eff8b2530b02274f384d2741a7
CindyYushin <Cindyhsiehyuhsin@gmail.com>
2025-03-25 20:35:07 +0800

    Create ex2-2.py

---
+++ ex2-2.py
@@ -0,0 +1,17 @@
+# this is my first program
+
+'''
+重要事項!
+這是我的第一個程式!!!
+........................
+'''
+
+print ('Hello world")
+
+# this is my first program
+
+print(8+7)
+
+print(5-4)
+
+print(6*2)
