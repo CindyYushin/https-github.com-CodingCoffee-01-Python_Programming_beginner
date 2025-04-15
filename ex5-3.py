7b732ba4215682a83d697a79d5f9a7ac89a7c8b6
CindyYushin <Cindyhsiehyuhsin@gmail.com>
2025-04-15 20:49:53 +0800

    ex5-3.py

---
+++ ex5-3.py
@@ -0,0 +1,14 @@
+
+x = int(input ("please input num x :"))
+y = int(input ("please input num y :"))
+if (x > y ):
+    print("x > y")
+elif x < y:
+    print ("x < y ")  
+else:
+    print("x ==y ")
+'''
+x = 10
+if x > 5:
+    print("x is greater than 5")
+'''
