5ba165683584b612cbda29f18b129c4d778402cb
CindyYushin <Cindyhsiehyuhsin@gmail.com>
2025-04-22 21:14:31 +0800

    ex6-9.py

--- ex6-9.py
+++ ex6-9.py
@@ -1,7 +1,9 @@
-radius = float(input("請輸入圓的半徑："))
-
-area = math.pi * radius ** 2
-circumference = 2 * math.pi * radius
-
-print(f"圓的面積為：{area:.2f}")
-print(f"圓的周長為：{circumference:.2f}")
+x=int(input("please input a num:"))
+y=int(input("please input a num:"))
+r=float(input("please input a num radius:"))
+print("x = ",x)
+print("y = ",y)
+print("r = ",r)
+print("x + y= ",x+y)
+print("面積= ",3.14*r*r)
+print("周長=",2*3.14*r)
