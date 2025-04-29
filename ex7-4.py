df50708265aa449f7a2277ef71266377c573733f
CindyYushin <Cindyhsiehyuhsin@gmail.com>
2025-04-29 19:55:49 +0800

    Create ex7-4.py

---
+++ ex7-4.py
@@ -0,0 +1,14 @@
+def add(x, y):
+    return x + y
+def mul(x, y):
+    return x * y
+def sub(x, y):
+    return x - y
+def div(x, y):
+    return x / y   
+x = int(input("please input num x:"))
+y = int(input("please input num y:"))
+print("x+y =",add(x,y))
+print("x*y =",mul(x,y))
+print("x-y =",sub(x,y))
+print("x/y =",div(x,y))
